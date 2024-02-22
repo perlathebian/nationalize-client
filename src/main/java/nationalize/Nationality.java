7b4fb519bf720212c970c71bad035eddc2b5a655
Perla Thebian <perlathebian02@gmail.com>
2024-02-22 11:32:10 +0100

    build: add the commons-lang3 dependency to pom.xml

--- src/main/java/nationalize/Nationality.java
+++ src/main/java/nationalize/Nationality.java
@@ -4,7 +4,7 @@ import com.google.gson.annotations.SerializedName;
 
 import java.util.List;
 
-public class Nationality {
+public class  Nationality {
 
     private long count;
     private String name;
